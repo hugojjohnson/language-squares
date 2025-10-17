c88b59369702b3a9be79856ab26003be960699b6
Hugo Johnson <hugojohnson271@gmail.com>
2025-10-17 22:04:08 +1100

    finally figured images out

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '/': path.resolve(__dirname, 'public'),
+      '@public': path.resolve(__dirname, 'public'),
     },
   },
 })
