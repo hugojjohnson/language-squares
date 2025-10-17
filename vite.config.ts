0a900fada4f45b1adf74e3e4b05fd5b5d9905353
Hugo Johnson <hugojohnson271@gmail.com>
2025-10-17 21:22:21 +1100

    added zod for much better build time checks

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { PUBLIC_FOLDER_PREFIX } from "./env/build"
 
 // esnext is a recent addition so it won't work on old browsers. However it lets you use
 // the bson library to create object ids. If you want to build for old browsers, just remove
@@ -8,7 +9,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "",
+  base: PUBLIC_FOLDER_PREFIX,
   build: {
     target: "esnext",
     outDir: 'build',
